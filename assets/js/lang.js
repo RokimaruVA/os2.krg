ac63f80f74071c5946a976e2c658c4142c1167c6
RokimaruVA <rokimarux@gmail.com>
2024-03-26 15:33:42 +0500

    0.240303 translation + animation optimisation

---
+++ assets/js/lang.js
@@ -0,0 +1,51 @@
+const radioButtons = document.querySelectorAll('input[type="radio"]');
+const allLang = ["kz", "ru", "en"]; // Порядок языков сохранен как в вашем исходном коде
+function changeHtmlLang(lang) {
+ document.documentElement.lang = lang;
+}
+radioButtons.forEach((button) => {
+ button.addEventListener("change", changeURLLanguage);
+});
+
+function changeURLLanguage() {
+ let lang = document.querySelector('input[name="language"]:checked').value; // Получаем значение выбранного языка
+ location.href = window.location.pathname + "#" + lang;
+ location.reload(); // Перезагружаем страницу после смены языка
+}
+function changeLanguage() {
+ let hash = window.location.hash.substr(1);
+ console.log(hash);
+ if (!allLang.includes(hash)) {
+  location.href = window.location.pathname + "#en";
+  location.reload();
+  hash = "en"; // Установка языка по умолчанию в случае некорректного значения в хеше
+ }
+ // Выбираем все радио-инпуты
+ document.querySelectorAll('input[type="radio"]').forEach((input) => {
+  // Помечаем как выбранный тот, у которого значение совпадает с хешем
+  input.checked = input.value === hash;
+ });
+ document.querySelector('input[value="' + hash + '"]').checked = true; // Выбираем соответствующий радио-инпут
+ document.querySelector("title").innerHTML = langArr["title"][hash];
+ document
+  .querySelector('meta[name="description"]')
+  .setAttribute("content", langArr["meta-description"][hash]);
+ document
+  .querySelector('meta[name="keywords"]')
+  .setAttribute("content", langArr["meta-keywords"][hash]);
+ document
+  .querySelector('meta[property="og:title"]')
+  .setAttribute("content", langArr["title"][hash]);
+ document
+  .querySelector('meta[property="og:description"]')
+  .setAttribute("content", langArr["meta-description"][hash]);
+ changeHtmlLang(hash);
+ for (let key in langArr) {
+  let elem = document.querySelector(".lang-" + key);
+  if (elem) {
+   elem.innerHTML = langArr[key][hash];
+  }
+ }
+}
+
+changeLanguage();
---
+++ assets/js/translations.js
@@ -0,0 +1,134 @@
+const langArr = {
+ "title": {
+  "kz": "№2 Оқушылар сарайы",
+  "ru": "Дворец школьников №2. Образование для детей.",
+  "en": "Palace of Schoolchildren №2. Education for children.",
+ },
+ "meta-description": {
+  "kz":
+   "Біздің ұйымымыз мектеп және бала ағаларына қосымша білім береді. Бізде сіздің баланыздың мүйіздеріне сәйкес, оның мәңгілікті қызығушыларына арналған білім бөлімдерін, клубтарды және шегіншілерді таба аласыз.",
+  "ru":
+   "Наша организация предоставляет дополнительное образование для детей школьного и дошкольного возраста. У нас вы найдете образовательные секции, клубы и кружки, адаптированные под интересы вашего ребенка.",
+  "en":
+   "Our organization provides supplementary education for children of school and preschool age. You can find educational sections, clubs, and circles tailored to your child's interests with us.",
+ },
+ "meta-keywords": {
+  "kz": "",
+  "ru":
+   "Дополнительное образование для детей, образовательные программы, внеурочная деятельность, детские курсы, развивающие кружки, обучение и развитие навыков, спортивные секции, танцевальные курсы, художественные занятия, музыкальные занятия, курсы по рисованию, компьютерные занятия, языковые курсы, театральные мастер-классы, научные эксперименты, робототехника, арт-терапия, футбол, баскетбол, волейбол, балет, современные танцы, хип-хоп, рисование, лепка, декоративное искусство",
+  "en":
+   "Supplementary education, After-school programs, Enrichment programs, Educational activities, Extracurricular activities, Learning resources, Academic enrichment, Skill development, Creative learning, Enrichment classes, Educational workshops, Specialized education, Learning centers, Enrichment courses, Academic support, Tutoring services, STEM education, Arts education, Physical education, Language learning",
+ },
+ "": {
+  "kz": "",
+  "ru": "",
+  "en": "",
+ },
+
+ "city": {
+  "kz": "Қарағанды қаласы",
+  "ru": "город Караганда",
+  "en": "Karaganda city",
+ },
+ "organization-name": {
+  "kz": "№2 Оқушылар сарайы",
+  "ru": "Дворец школьников №2",
+  "en": "The Palace of Schoolchildren №2",
+ },
+ "nav-about": {
+  "kz": "Біз туралы",
+  "ru": "О нас",
+  "en": "About us",
+ },
+ "nav-sign-in": {
+  "kz": "",
+  "ru": "Как записаться",
+  "en": "How to sign up",
+ },
+ "nav-direction": {
+  "kz": "",
+  "ru": "Чему мы учим",
+  "en": "What do we teach",
+ },
+ "nav-news": {
+  "kz": "",
+  "ru": "Новости и события",
+  "en": "News and Events",
+ },
+ "nav-qna": {
+  "kz": "",
+  "ru": "Вопросы и ответы",
+  "en": "Q&A Section",
+ },
+ "nav-competitions": {
+  "kz": "",
+  "ru": "Соревнования",
+  "en": "Competitions",
+ },
+ "nav-magazine": {
+  "kz": "",
+  "ru": "Журнал",
+  "en": "Our magazine",
+ },
+ "nav-seminars": {
+  "kz": "",
+  "ru": "Семинары",
+  "en": "Seminars",
+ },
+ "nav-team": {
+  "kz": "",
+  "ru": "Коллектив",
+  "en": "Team",
+ },
+ "nav-documents": {
+  "kz": "",
+  "ru": "Документация",
+  "en": "Documentation",
+ },
+ "nav-vacancies": {
+  "kz": "",
+  "ru": "Вакансии",
+  "en": "Vacancies",
+ },
+ "header-about": {
+  "kz": "Біз туралы",
+  "ru": "О нас",
+  "en": "About Us",
+ },
+ "about-paragraph": {
+  "kz":
+   " №2 оқушылар сарайы - өз қызметін оқу жылы және демалыс кезеңінде жүзеге асыратын көпсалалы мекеме. Біздің үйірмешілер өздерінің шығармашылық қабілеттерін дамытып,ой-өрісін кеңейтіп, жаңа дағдыларды үйреніп қана қоймай, өздеріне жаңа достар табады.",
+  "ru":
+   "Дворец школьников №2 - это многопрофильное учреждение,	осуществляющее свою деятельность во время учебного года и в	каникулярный период. Наши учащиеся развивают свои творческие способности, расширяют кругозор, изучают новые навыки и находят новых друзей. ",
+  "en":
+   "The Palace of Schoolchildren №2 is a multifunctional institution that operates during the school year and during vacation periods. Our students develop their creative abilities, broaden their horizons, learn new skills, and make new friends.",
+ },
+ "header-contacts": {
+  "kz": "Байланыстар",
+  "ru": "Контакты",
+  "en": "Contacts",
+ },
+ "edu-depart-link": {
+  "kz": "Білім бөлімі",
+  "ru": "Отдел образования",
+  "en": "Education Department",
+ },
+ "address": {
+  "kz": "141 есеп кварталы, 690 ст-е",
+  "ru": "141-учёт. квартал, ст. 690",
+  "en": "141 Acc. Qtr., Bldg. 690",
+ },
+ "our-location": {
+  "kz":
+   " Біз қаланың оңтүстік-шығысында, Сарыарқа көшесі мен Шахтерлер даңғылының арасында, &quotДостық үйінің&quot ар жағында орналасқанбыз. Сізді көруге әрқашан қуаныштымыз! ",
+  "ru":
+   " Мы находимся на юго-востоке города, между улицей Сарыарка и проспектом шахтёров, за &quotДомом дружбы народов&quot. Вход расположен со стороны улицы Сарыарка. Мы будем вам рады! ",
+  "en":
+   "We are located in the southeast of the city, between Sarayarka Street and Shahterov Avenue, behind the &quotHouse of Friendship of Nations&quot. The entrance is located on Sarayarka Street side. We will be glad to see you! ",
+ },
+ "": {
+  "kz": "",
+  "ru": "",
+  "en": "",
+ },
+};
